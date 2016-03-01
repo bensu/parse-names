e97493be91cbe9c3cce0109457327d48b92b3e01
Sebastian Bensusan <sbensu@gmail.com>
2016-03-01 18:59:48 +0100

    Initial commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to parse-names
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/parse_names/core_test.clj
@@ -0,0 +1,7 @@
+(ns parse-names.core-test
+  (:require [clojure.test :refer :all]
+            [parse-names.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
